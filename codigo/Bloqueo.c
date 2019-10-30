d11aaa6cf6fdce819e7806b9c13944248a73f592
ggzor <30713864+ggzor@users.noreply.github.com>
2019-10-30 03:59:51 -0600

    Terminando presentación

---
+++ codigo/Bloqueo.c
@@ -0,0 +1,50 @@
+#include <gtk/gtk.h>
+#include <unistd.h>
+
+static void click(GtkWidget *button, gpointer user_data) {
+  sleep(2);
+  printf("Terminando espera\n");
+}
+
+static void click2(GtkWidget *button, gpointer user_data) {
+  printf("Click!\n");
+}
+
+static void activate(GtkApplication* app, gpointer user_data) {
+  GtkWidget *window = gtk_application_window_new(app);
+
+  GtkWidget *contenedor = gtk_fixed_new();
+  gtk_container_add(GTK_CONTAINER(window), contenedor);
+
+  GtkWidget *imagen = gtk_image_new_from_file("Imagen.gif");
+  gtk_fixed_put(GTK_FIXED(contenedor), imagen, 0, 0);
+
+  GtkWidget *buttonBox = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+  gtk_fixed_put(GTK_FIXED(contenedor), buttonBox, 50, 50);
+
+  GtkWidget *button = gtk_button_new_with_label("Bloquear");
+  gtk_container_add(GTK_CONTAINER(buttonBox), button);
+
+  GtkWidget *buttonBox2 = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+  gtk_fixed_put(GTK_FIXED(contenedor), buttonBox2, 50, 150);
+
+  GtkWidget *button2 = gtk_button_new_with_label("Clickeame");
+  gtk_container_add(GTK_CONTAINER(buttonBox), button2);
+
+  g_signal_connect(button, "clicked", G_CALLBACK(click), NULL);
+  g_signal_connect(button2, "clicked", G_CALLBACK(click2), NULL);
+
+  gtk_window_set_title (GTK_WINDOW(window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW(window), 200, 200);
+  gtk_widget_show_all(window);
+}
+
+int main(int argc, char **argv) {
+  GtkApplication *app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
+}
